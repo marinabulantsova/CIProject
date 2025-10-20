2cfdb527e6cebacc71894bb2555ac8482445281e
marinabulantsova <marinabulancova@mail.ru>
2025-10-20 16:06:26 +0300

    second actions

--- tests/test_button.py
+++ tests/test_button.py
@@ -4,3 +4,6 @@ def test_button_exist(driver):
     driver.get("https://www.qa-practice.com/elements/button/simple")
     assert driver.find_element(By.ID, "submit-id-submit").is_displayed()
 
+def test_button_exist_2(driver):
+    driver.get("https://www.qa-practice.com/elements/button/like_a_button")
+    assert driver.find_element(By.LINK_TEXT, "Click").is_displayed()
