4d8b54bb8dc4238c75a0d08e73bbd868d8f1b1cd
marinabulantsova <marinabulancova@mail.ru>
2025-10-21 08:55:32 +0300

    add allure

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,9 +1,16 @@
 from selenium.webdriver.common.by import By
+import allure
 
+@allure.feature("Buttons")
+@allure.story("Button")
 def test_button_exist(driver):
     driver.get("https://www.qa-practice.com/elements/button/simple")
-    assert driver.find_element(By.ID, "submit-id-submit").is_displayed()
+    with allure.step("Check button exist"):
+        assert driver.find_element(By.ID, "submit-id-submit").is_displayed()
 
+@allure.feature("Buttons")
+@allure.story("LikeAButton")
 def test_button_exist_2(driver):
     driver.get("https://www.qa-practice.com/elements/button/like_a_button")
-    assert driver.find_element(By.LINK_TEXT, "Click").is_displayed()
+    with allure.step("Check button exist"):
+        assert driver.find_element(By.LINK_TEXT, "Click").is_displayed()
