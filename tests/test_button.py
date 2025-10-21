89436ebf99898f8567111f4b40e8559c761d0518
marinabulantsova <marinabulancova@mail.ru>
2025-10-21 11:06:25 +0300

    add fail_test

--- tests/test_button.py
+++ tests/test_button.py
@@ -14,3 +14,8 @@ def test_button_exist_2(driver):
     driver.get("https://www.qa-practice.com/elements/button/like_a_button")
     with allure.step("Check button exist"):
         assert driver.find_element(By.LINK_TEXT, "Click").is_displayed()
+
+@allure.feature("FailTest")
+@allure.story("1 == 0")
+def test_fail(driver):
+    assert 1 == 0
